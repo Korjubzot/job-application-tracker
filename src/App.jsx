c7d216da25ca8be0e303ad2b77f176767b251b81
Korjubzot <korjubzot@gmail.com>
2024-09-21 13:19:39 +0200

    Added a table with basic data formats

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
 import JobListings from "./components/jobListings/jobListings";
 
+let data = {
+  company: "Lionbridge",
+  role: "QA Engineer",
+  salary: "25PLN/hr",
+  status: "Blank",
+};
+
+// let data2 = {
+//   company: "Starlike",
+//   role: "Consultant",
+//   salary: "10,000GBP/m",
+//   status: "Blank",
+// };
+
 function App() {
   return (
     <>
-      <JobListings />
+      <h1>Job Application Tracker</h1>
+      <p>
+        You can use this tracker to input data on your job applications. Add the
+        company, role, salary, and current status.
+      </p>
+      <JobListings data={data} />
     </>
   );
 }
--- src/components/jobListings/jobListings.jsx
+++ src/components/jobListings/jobListings.jsx
@@ -1,9 +1,34 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 // TODO figure out why eslint is causing an error here, probably just some misconfiguration or a missing rc file
 
-function JobListings() {
-  return <div>jobListings</div>;
+function JobListings({ data }) {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Role</th>
+          <th>Salary</th>
+          <th>Status</th>
+        </tr>
+        <tr>
+          <td>{data.company}</td>
+          <td>{data.role}</td>
+          <td>{data.salary}</td>
+          <td>
+            <select name="status" id="status">
+              <option value="applied">Applied</option>
+              <option value="rejected">Rejected</option>
+              <option value="interviewing">Interviewing</option>
+              <option value="accepted">Accepted</option>
+            </select>
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 export default JobListings;
